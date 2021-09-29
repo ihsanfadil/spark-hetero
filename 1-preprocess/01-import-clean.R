77fdeffbcff8a937b9605095d4a513b64a1fa166
ihsanfadil <ihsn.fdlh@gmail.com>
2021-09-29 16:42:55 +0700

    started a new repo for malaria heterogeneity.

---
+++ 1-preprocess/01-import-clean.R
@@ -0,0 +1,20 @@
+
+# Preamble ----------------------------------------------------------------
+
+# Code author  : Ihsan Fadilah
+# Last updated : Late September 2021
+# Project      : Recent characteristics and spatial heterogeneity of notified
+#                malaria cases in the Papua region of Indonesia
+
+# About
+# This script provides code from importing to further cleaning the data for the
+# next steps of analysis.
+
+# Setup ----------------------------------------------------------------------------------
+
+library(tidyverse)  # Tidy and readable code
+library(lubridate)  # Handle time objects
+library(labelled)   # Label variables
+library(readxl)     # Read in Excel files
+library(here)       # Navigate files
+
